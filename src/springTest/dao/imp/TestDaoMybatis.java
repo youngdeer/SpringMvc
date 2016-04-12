ba23d7a6d66b9fd4ace5e2883167bc2cd56e5257
youngDeer <young_deer@sina.com>
2016-04-12 13:11:05 +0800

    增加事务控制

--- src/springTest/dao/imp/TestDaoMybatis.java
+++ src/springTest/dao/imp/TestDaoMybatis.java
@@ -1,13 +1,26 @@
 package springTest.dao.imp;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
+@SuppressWarnings("unchecked")
 public class TestDaoMybatis extends SqlSessionDaoSupport{
 
-	public List<String> list(){
+	
+	public List<HashMap> list(){
 		return this.getSqlSession().selectList("springTest.dao.mapper.listAll");
+	}
+	
+	public int deleteById(String id){
+		int i = this.getSqlSession().delete("springTest.dao.mapper.deleteById", id);
+		int j = 1/0;
+		return i;
+	}
+	
+	public int insert(HashMap map){
+		return this.getSqlSession().insert("springTest.dao.mapper.insert", map);
 	}
 	
 
--- src/springTest/test/mybatisTest.java
+++ src/springTest/test/mybatisTest.java
@@ -1,15 +1,23 @@
 package springTest.test;
 
+import java.util.HashMap;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import springTest.dao.imp.TestDaoMybatis;
 
+@SuppressWarnings("unchecked")
 public class mybatisTest {
 
 	public static void main(String[] args){
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		TestDaoMybatis obj = (TestDaoMybatis) context.getBean("testDaoMybatis");
 		System.out.println(obj.list());
+		HashMap map = new HashMap();
+		map.put("id", 5);
+		map.put("msg", "eee");
+		obj.insert(map);
+//		obj.deleteById("4");
 	}
 }
