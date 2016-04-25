e54cfc0381292b093723b890f84d72314fd3952b
youngDeer <young_deer@sina.com>
2016-04-25 14:08:19 +0800

    增加过滤器

---
+++ src/deerSYS/common/ValidateFilter.java
@@ -0,0 +1,37 @@
+package deerSYS.common;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class ValidateFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpSession session = request.getSession();
+		//此处可以验证登陆状态
+		System.out.println(session.getAttribute("username"));
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
