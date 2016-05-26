94537e83c8339a91f607623b36c5d571f4876831
youngDeer <young_deer@sina.com>
2016-05-26 18:14:58 +0800

    test

--- src/io/FileExample.java
+++ src/io/FileExample.java
@@ -10,6 +10,7 @@ public class FileExample {
 	
 	public static void createFile(){
 		File f = new File("E:/example/deer.txt");
+		File f1 = new File("E:"+File.separator+"example");
 		try{
 			f.createNewFile();
 			System.out.println("返回分区大小"+f.getTotalSpace()/(1024*1024*1024)+"G");
@@ -17,6 +18,12 @@ public class FileExample {
 			//f.delete();
 			System.out.println("文件名："+f.getName());
 			System.out.println("父级目录："+f.getParent());
+			
+			//遍历目录下全部文件
+			File[] listFile = f1.listFiles();
+			for(File str : listFile){
+				System.out.println(str);
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
